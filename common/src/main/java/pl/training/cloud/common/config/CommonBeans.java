898e689cc453ab707b520dff243801371344fa9c
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-07-10 14:13:12 +0200

    Discovery

--- common/src/main/java/pl/training/cloud/common/config/CommonBeans.java
+++ common/src/main/java/pl/training/cloud/common/config/CommonBeans.java
@@ -1,10 +1,12 @@
 package pl.training.cloud.common.config;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.training.cloud.common.Mapper;
 
+@EnableDiscoveryClient
 @Configuration
 public class CommonBeans {
 
