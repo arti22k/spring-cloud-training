e81476e7f09723f027e392f61729ecf2975c929c
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-07-10 10:33:59 +0200

    Base configuration

---
+++ src/main/java/pl/training/cloud/CloudMicroservice.java
@@ -0,0 +1,13 @@
+package pl.training.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMicroservice {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMicroservice.class);
+    }
+
+}
---
+++ src/main/java/pl/training/cloud/controller/CloudController.java
@@ -0,0 +1,16 @@
+package pl.training.cloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CloudController {
+
+    @RequestMapping("status")
+    public String getStatus() {
+        return "Cloud is running...";
+    }
+
+}
