36e69e45adc8c76be0e51a3dcf8b0f03fb863b13
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-07-11 19:55:59 +0200

    Refactorings

---
+++ organization/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "******************************************************"
+echo "Waiting for the Discovery server to start on port 8080"
+echo "******************************************************"
+while ! `nc -z discovery 8080`; do sleep 3; done
+echo ">>>>>>>>>>>> Discovery Server has started"
+
+echo "****************************************************"
+echo "Waiting for the Config server to start on port 8080"
+echo "****************************************************"
+while ! `nc -z configuration 8080`; do sleep 3; done
+echo ">>>>>>>>>>>> Config server has started"
+
+echo "*****************************************************"
+echo "Waiting for the Database server to start on port 5432"
+echo "*****************************************************"
+while ! `nc -z users_database 5432`; do sleep 3; done
+echo ">>>>>>>>>>>> Database Server has started"
+
+java -jar -Dspring.cloud.config.uri=$CONFIG_URI /usr/local/service/users.jar
