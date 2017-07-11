7d63f0c424e043e79f8af4614fbec9a4bb7bbd1a
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-07-11 09:34:45 +0200

    Service to service communication

--- users/src/main/java/pl/training/cloud/users/config/Beans.java
+++ users/src/main/java/pl/training/cloud/users/config/Beans.java
@@ -3,8 +3,6 @@ package pl.training.cloud.users.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import pl.training.cloud.users.repository.UsersRepository;
 import pl.training.cloud.users.service.UsersService;
 
@@ -15,11 +13,6 @@ public class Beans {
     @Bean
     public UsersService usersService(UsersRepository usersRepository) {
         return new UsersService(usersRepository);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new StandardPasswordEncoder();
     }
 
 }
