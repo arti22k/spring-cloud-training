495d3ce736c086524ff0be6897d7279840018f1c
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-07-12 13:35:35 +0200

    Docker

--- users/run.sh
+++ users/run.sh
@@ -24,10 +24,4 @@ echo "*****************************************************"
 while ! `nc -z rabbitmq 5672`; do sleep 3; done
 echo ">>>>>>>>>>>> RabbitMQ server has started"
 
-echo "***************************************************"
-echo "Waiting for the Kafka server to start on port 9092"
-echo "***************************************************"
-while ! `nc -z kafka 9092`; do sleep 3; done
-echo ">>>>>>>>>>>> Kafka server has started"
-
-java -jar -Dcloud.stream.bindings.kafka.binder.zkNodes=$KAFKA_HOST -Dcloud.stream.bindings.kafka.binder.brokers=$KAFKA_HOST -Dspring.cloud.config.uri=$CONFIG_URI -Dspring.rabbitmq.host=$RABBITMQ_HOST -Dspring.rabbitmq.username=$RABBITMQ_USERNAME -Dspring.rabbitmq.password=$RABBITMQ_PASSWORD /usr/local/service/users.jar
+java -jar -Dspring.cloud.stream.bindings.kafka.binder.zkNodes=$KAFKA_HOST -Dspring.cloud.stream.bindings.kafka.binder.brokers=$KAFKA_HOST -Dspring.cloud.config.uri=$CONFIG_URI -Dspring.rabbitmq.host=$RABBITMQ_HOST -Dspring.rabbitmq.username=$RABBITMQ_USERNAME -Dspring.rabbitmq.password=$RABBITMQ_PASSWORD /usr/local/service/users.jar
